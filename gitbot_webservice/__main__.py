3bb8a5129ac8c89e2895d7784bbf22f6bcf31290
small yellow duck <kiri.nichol@gmail.com>
2018-05-05 18:41:35 -0400

    added function to leave a comment whenever someone opens a github issue

--- gitbot_webservice/__main__.py
+++ gitbot_webservice/__main__.py
@@ -1,16 +1,56 @@
 import os
+import aiohttp
 
 from aiohttp import web
 
-async def main(request):
-    return web.Response(status=200, text="Hello world!")
+from gidgethub import routing, sansio
+from gidgethub import aiohttp as gh_aiohttp
 
+
+
+routing = routing.Router()
+
+@router.register("issues", action="opened")
+async def issue_opened_event(event, gh, *args, **kwargs):
+    """ Whenever an issue is opened, greet the author and say thanks."""
+    url = event.data["issue"]["comments_url"]
+    author = event.data["user"]["login"]
+    message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
+    await gh.post(url, data={"body": message})
+
+
+# async def main(request):
+#     return web.Response(status=200, text="Hello world!")
+
+async def main(request):
+    # read the GitHub webhook payload
+    body = await request.read()
+
+    # our authentication token and secret
+    secret = os.environ.get("GH_SECRET")
+    oauth_token = os.environ.get("GH_AUTH")
+
+    # a representation of GitHub webhook event
+    event = sansio.Event.from_http(request.headers, body, secret=secret)
+
+    # instead of mariatta, use your own username
+    async with aiohttp.ClientSession() as session:
+        gh = gh_aiohttp.GitHubAPI(session, "mariatta",
+                                  oauth_token=oauth_token)
+
+        # call the appropriate callback for the event
+        await router.dispatch(event, gh)
 
 if __name__ == "__main__":
     app = web.Application()
-    app.router.add_get("/", main)
+    app.router.add_post("/", main)
     port = os.environ.get("PORT")
     if port is not None:
         port = int(port)
 
     web.run_app(app, port=port)
+
+
+
+    # return a "Success"
+    return web.Response(status=200)
