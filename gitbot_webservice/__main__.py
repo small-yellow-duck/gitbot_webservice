b865a66e1c17309acb9402cc5ac2b6d686de21ef
small yellow duck <kiri.nichol@gmail.com>
2018-05-05 18:51:36 -0400

    fixed username in main

--- gitbot_webservice/__main__.py
+++ gitbot_webservice/__main__.py
@@ -35,7 +35,7 @@ async def main(request):
 
     # instead of mariatta, use your own username
     async with aiohttp.ClientSession() as session:
-        gh = gh_aiohttp.GitHubAPI(session, "mariatta",
+        gh = gh_aiohttp.GitHubAPI(session, "small-yellow-duck",
                                   oauth_token=oauth_token)
 
         # call the appropriate callback for the event
