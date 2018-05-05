e3ae8072759d497b6d77cb18ad48e2b1b063adde
small yellow duck <kiri.nichol@gmail.com>
2018-05-05 19:51:33 -0400

    fixed bug in author definition

--- gitbot_webservice/__main__.py
+++ gitbot_webservice/__main__.py
@@ -14,7 +14,7 @@ router = routing.Router()
 async def issue_opened_event(event, gh, *args, **kwargs):
     """ Whenever an issue is opened, greet the author and say thanks."""
     url = event.data["issue"]["comments_url"]
-    author = event.data["user"]["login"]
+    author = event.data["issue"]["user"]["login"]
     message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
     await gh.post(url, data={"body": message})
 
